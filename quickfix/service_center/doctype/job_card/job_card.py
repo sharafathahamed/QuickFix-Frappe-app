0f84eac38754f90e2b2f2faa985af41aca22ed8f
sharafathahamed <sarafshadh2004@gmail.com>
2026-02-26 10:38:53 +0530

    fix: add validate to job card

--- quickfix/service_center/doctype/job_card/job_card.py
+++ quickfix/service_center/doctype/job_card/job_card.py
@@ -3,9 +3,29 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class JobCard(Document):
-	def before_insert(self):
+	def validate(self):
+		if len(self.customer_phone)!=10:
+			frappe.throw("Number should be exactly 10")
+		if self.status not in ["Draft","Pending Diagnosis","Awaiting Customer Approval"] and not self.assigned_technician:
+			frappe.throw("Assign Technician First!!")
 		settings=frappe.db.get_single_value("QuickFix Settings","default_labour_change")
 		self.labour_charge=settings
+		self.calculate_total()
+	def calculate_total(self):
+		parts_total=0
+		for row in self.parts_used:
+			row.total_price=row.quantity*row.unit_price
+			parts_total+=row.total_price
+		self.parts_total=parts_total
+		self.final_amount = self.parts_total + self.labour_charge
+	def before_submit(self):
+		if self.status!="Ready for Delivery":
+			frappe.throw("Can't Submit without 'Ready for Delivery'")
+		for row in self.parts_used:
+			curr_stck=frappe.db.get_value("Spare Part",row.part,"sock_qty")
+			if flt(curr_stck)<flt(row.quantity):
+				frappe.throw(f"Insufficient Stock for {row.part_name}. Available: {curr_stck}, Required: {row.quantity}")
